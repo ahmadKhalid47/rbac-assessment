c9ec0d24d2010908e023a9dcbd5f299a5b99d212
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-01-22 04:48:41 +0500

    update Navbar.js

--- frontend/src/components/Common/Navbar.js
+++ frontend/src/components/Common/Navbar.js
@@ -1,16 +1,18 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import LogoutButton from "./Logout";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const userData = useSelector((state) => state.users);
+  console.log(userData);
 
   return (
     <nav class="bg-gray-800 p-4">
       <div class="max-w-7xl mx-auto flex items-center justify-between">
-        <div class="text-white text-lg font-semibold">
-          {userData?.name}- {userData?.role}
-        </div>
+        <Link to={"/dashboard"} class="text-white text-lg font-semibold">
+          {userData?.name} - {userData?.role}
+        </Link>
         <LogoutButton />
       </div>
     </nav>
