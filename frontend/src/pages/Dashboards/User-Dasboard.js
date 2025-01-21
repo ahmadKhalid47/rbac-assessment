f02aa1f9faab3a7e3eff80a7596bee7800ca2c2d
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-01-22 03:14:00 +0500

    update postController.js, DisplayPosts.js and User-Dasboard.js

--- frontend/src/pages/Dashboards/User-Dasboard.js
+++ frontend/src/pages/Dashboards/User-Dasboard.js
@@ -1,10 +1,15 @@
 import PostGrid from "components/Posts/DisplayPosts";
 import CreatePost from "components/Posts/CreatePost";
-import React from "react";
+import React, { useState } from "react";
 import { useSelector } from "react-redux";
 
 const UserDashboard = () => {
   const userData = useSelector((state) => state.users);
+  const [showPosts, setShowPosts] = useState(false);
+
+  const handleToggle = () => {
+    setShowPosts(!showPosts);
+  };
 
   return (
     <div className="p-6">
@@ -12,8 +17,26 @@ const UserDashboard = () => {
       <p className="text-gray-700">
         Welcome User! Here you can manage your posts.
       </p>
-      <CreatePost />
-      <PostGrid userId={userData?._id} isAdmin={false} />
+
+      <div className="w-full h-fit flex my-6 text-2xl font-bold p-2 justify-around ">
+        <button
+          className={`w-[49%] p-3 text-center rounded-l-xl ${!showPosts ? "bg-gray-100" : "bg-blue-500 text-white"}`}
+          onClick={handleToggle}
+        >
+          All Posts
+        </button>
+        <button
+          className={`w-[49%] p-3 text-center rounded-r-xl ${showPosts ? "bg-gray-100" : "bg-blue-500 text-white"}`}
+          onClick={handleToggle}
+        >
+          Create Post
+        </button>
+      </div>
+      {!showPosts ? (
+        <CreatePost />
+      ) : (
+        <PostGrid userId={userData?._id} isAdmin={false} />
+      )}
     </div>
   );
 };
