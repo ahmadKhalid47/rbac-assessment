e899fa0bc7fc3c1f16eff6f747e762fe6e37fe15
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-01-22 00:02:18 +0500

    update userController.js auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,45 +2,6 @@ const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 const User = require("../models/user.model");
 
-const register = async (req, res) => {
-  try {
-    const { name, email, password, role } = req.body;
-
-    if (!["Admin", "User"].includes(role)) {
-      return res.status(400).json({ message: "Invalid role" });
-    }
-
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-
-    const newUser = new User({ name, email, password: hashedPassword, role });
-    await newUser.save();
-
-    const token = jwt.sign(
-      { userId: newUser._id, role: newUser.role },
-      process.env.JWT_SECRET,
-      { expiresIn: "1h" }
-    );
-
-    res.status(201).json({
-      message: "User created successfully",
-      token,
-      user: {
-        id: newUser._id,
-        name: newUser.name,
-        email: newUser.email,
-        role: newUser.role,
-      },
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ message: "Server error" });
-  }
-};
 
 const login = async (req, res) => {
   try {
