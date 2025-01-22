9159a15c312a941ce7d58d92b7ca9e862bff2c3a
ahmadKhalid47 <ahmadrazakhalid9.0@gmail.com>
2025-01-22 12:29:39 +0500

    min, max

--- backend/middlewares/verifyTokenMiddleware.js
+++ backend/middlewares/verifyTokenMiddleware.js
@@ -1,11 +1,13 @@
 const jwt = require("jsonwebtoken");
 const userModel = require("../models/user.model");
 
-// verify token middleware to verify every api  
+// verify token middleware to verify every api
 
 const verifyTokenMiddleware = async (req, res, next) => {
   const token = req.cookies.token;
-    if (!token) {      
+  console.log("token......", token);
+  
+  if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
   try {
@@ -15,7 +17,7 @@ const verifyTokenMiddleware = async (req, res, next) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-      req.user = user;
+    req.user = user;
     next();
   } catch (error) {
     return res.status(401).json({ message: "Invalid token" });
